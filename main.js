688439fa62ffb2328e5af8706f9635f635718cc7
Sho <cozymncfc0401@gmail.com>
2025-10-19 11:14:13 +0900

    making constructors into classes

--- main.js
+++ main.js
@@ -1,64 +1,82 @@
-const myLibrary = [];
+class Book {
+    constructor(title, author, page, status) {
+        this.id = crypto.randomUUID();
+        this.title = title;
+        this.author = author;
+        this.page = page;
+        this.read = status;
+    }
 
-function Book(title, author, page, status) {
-    this.id = crypto.randomUUID();
-    this.title = title;
-    this.author = author;
-    this.page = page;
-    this.read = status;
+    toggleReadStatus() {
+        this.read = !this.read;
+    }
 }
 
-Book.prototype.toggleReadStatus = function () {
-    this.read = !this.read;
-}
+class Library {
+    constructor() {
+        this.books = [];
+    }
 
-function addBookToLibrary(title, author, page, status) {
-    const newBook = new Book(title, author, page, status);
-    myLibrary.push(newBook);
-}
+    addBook(title, author, page, status) {
+        const newBook = new Book(title, author, page, status);
+        this.books.push(newBook);
+        this.displayLibrary();
+    }
 
-displayLibrary();
+    removeBook(id) {
+        const index = this.books.findIndex(book => book.id === id);
+        if (index != -1) {
+            this.books.splice(index, 1);
+        }
+        this.displayLibrary();
+    }
 
-function displayLibrary() {
-    const libraryDiv = document.getElementById('library');
-    libraryDiv.innerHTML = '<button id="newBookBtn">New Book</button>';
+    displayLibrary() {
+        const libraryDiv = document.getElementById('library');
+        libraryDiv.innerHTML = '<button id="newBookBtn">New Book</button>';
+    
+        this.books.forEach(book => {
+            // create a card
+            const card = document.createElement('div');
+            card.classList.add('book-card');
+            card.innerHTML = `
+                <h3>${book.title}</h3>
+                <p>Author: ${book.author}</p>
+                <p>Pages: ${book.page}</p>
+                <p>Status: <span class="status">${book.read}</span></p>
+                <button class="toggle-btn">Toggle Read Status</button>
+                <button class="remove-btn">Remove</button>
+            `;
+            libraryDiv.appendChild(card);
+    
+            // remove button
+            const removeBtn = card.querySelector('.remove-btn');
+            removeBtn.addEventListener('click', () => {
+                removeBook(book.id);
+            })
 
-    myLibrary.forEach(book => {
-        // create a card
-        const card = document.createElement('div');
-        card.classList.add('book-card');
-        card.innerHTML = `
-            <h3>${book.title}</h3>
-            <p>Author: ${book.author}</p>
-            <p>Pages: ${book.page}</p>
-            <p>Status: <span class="status">${book.read}</span></p>
-            <button class="toggle-btn">Toggle Read Status</button>
-            <button class="remove-btn">Remove</button>
-        `;
-        libraryDiv.appendChild(card);
+            // toggle button
+            const toggleBtn = card.querySelector('.toggle-btn');
+            const statusSpan = card.querySelector('.status');
+            toggleBtn.addEventListener('click', () => {
+                book.toggleReadStatus();
+                statusSpan.textContent = book.read;
+            });
 
-        // remove button
-        const removeBtn = card.querySelector('.remove-btn');
-        removeBtn.addEventListener('click', () => {
-            removeBook(book.id);
-        })
-
-        const toggleBtn = card.querySelector('.toggle-btn');
-        const statusSpan = card.querySelector('.status');
-        toggleBtn.addEventListener('click', () => {
-            book.toggleReadStatus();
-            statusSpan.textContent = book.read;
+            // new button
+            const newBookBtn = document.getElementById('newBookBtn');
+            newBookBtn.addEventListener('click', () => {
+                bookForm.showModal(); // show the modal
+            });
         });
-    });
+    }
 }
 
-const newBookBtn = document.getElementById('newBookBtn');
+const library = new Library();
+library.displayLibrary();
+
 const bookForm = document.getElementById('bookForm');
 const addBookForm = document.getElementById('addBookForm');
-
-newBookBtn.addEventListener('click', () => {
-    bookForm.showModal(); // show the modal
-});
 
 addBookForm.addEventListener('submit', (event) => {
     event.preventDefault(); // prevent default load and data processing
@@ -69,16 +87,7 @@ addBookForm.addEventListener('submit', (event) => {
     const page = formData.get('page');
     const status = formData.get('status') ? 'read' : 'not read yet';
 
-    addBookToLibrary(title, author, page, status);
-    displayLibrary();
+    library.addBook(title, author, page, status);
     bookForm.close();
     addBookForm.reset();
 });
-
-function removeBook(id) {
-    const index = myLibrary.findIndex(book => book.id === id);
-    if (index != -1) {
-        myLibrary.splice(index, 1);
-    }
-    displayLibrary();
-}
